713eb0fab16fb5de3ee05ea81ffe10e6409933cc
DADEVFA <davidfarah193@gmail.com>
2021-07-02 21:25:35 +0000

    Binding audio path from DB to lessons

--- static/js/script.js
+++ static/js/script.js
@@ -3,12 +3,3 @@ $(document).ready(function () {
   $('select').formSelect();
   $('.tooltipped').tooltip();
 });
-
-$('.pushpin-demo-nav').each(function () {
-  var $this = $(this);
-  var $target = $('#' + $(this).attr('data-target'));
-  $this.pushpin({
-    top: $target.offset().top,
-    bottom: $target.offset().top + $target.outerHeight() - $this.height()
-  });
-});
