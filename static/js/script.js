56bf4f16a4fb192bceaf2024a4cf4a798558fd10
DADEVFA <davidfarah193@gmail.com>
2021-07-06 04:46:39 +0000

    Add more form input fields for Add Lesson

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,23 @@
 $(document).ready(function () {
-  $('.sidenav').sidenav({edge: "right", inDuration: "750"});
+  $('.sidenav').sidenav({
+    edge: "right",
+    inDuration: "750"
+  });
   $('select').formSelect();
   $('.tooltipped').tooltip();
+});
+
+var slider = document.getElementById('due_date');
+noUiSlider.create(slider, {
+ start: [20, 80],
+ connect: true,
+ step: 1,
+ orientation: 'horizontal',
+ range: {
+   'min': 0,
+   'max': 100
+ },
+ format: wNumb({
+   decimals: 0
+ })
 });
