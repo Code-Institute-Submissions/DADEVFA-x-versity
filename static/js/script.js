37acdbdc2ab4cd35f4f8cc58f6f0c60de7257f45
DADEVFA <davidfarah193@gmail.com>
2021-07-09 04:06:40 +0000

    Add Student Action layout on Lessons

--- static/js/script.js
+++ static/js/script.js
@@ -45,3 +45,11 @@ function submissionUpload() {
     $(element).addClass('toggle-sub-styles')
   }
 };
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: 'left'
+  });
+});
+      
